75dfb42f9cfb96a298d43c9365ff448996a58abb
evans <evans@saratoga.co.za>
2015-02-15 16:56:14 +0200

    scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,44 +1,107 @@
+#!/bin/bash
 
-c1curlget() {
+# util methods
+
+c1get() {
   uri=$1
+  echo -n "GET $uri as $user: "
   curl -s -k https://localhost:8443/$uri --key tmp/certs/evan.key --cert tmp/certs/evan.cert 
+  echo " (exitCode $?)"
 }
+
+c2get() {
+  uri=$1
+  user=$2
+  echo -n "GET $uri as $user: "
+  curl -s -k https://localhost:8443/$uri --key tmp/certs/$user.key --cert tmp/certs/$user.cert 
+  echo " (exitCode $?)"
+}
+
+c3post() {
+  uri=$1
+  user=$2
+  data=$3
+  echo -n "POST $uri as $user with data '$data': "
+  curl -s -k https://localhost:8443/$uri -d "$data" --key tmp/certs/$user.key --cert tmp/certs/$user.cert
+  echo " (exitCode $?)"
+}
+
+c1info() {
+  keyName=$1
+  c1get key/$keyName 
+}
+
+# keyName context
 
 keyName=testdek
 
-c0redisShow() {
-   echo; echo; echo + redis-cli keys 'dek:*'
-   redis-cli keys 'dek:*'
-   echo; echo + redis-cli hkeys "dek:$keyName"
-   redis-cli hkeys "dek:$keyName"
-   echo; echo + redis-cli redis hget "dek:$keyName"
-   redis-cli hget "dek:$keyName" 'secret:brent:evan'
-   redis-cli hget "dek:$keyName" 'secret:brent:henry'
-   redis-cli hget "dek:$keyName" 'secret:evan:henry'
+c0postsecret2() {
+  c3post secret/$keyName evan eee
+  c3post secret/$keyName henry eee
 }
 
+c0postsecret3() {
+  c3post secret/$keyName evan eee
+  c3post secret/$keyName henry eee
+  c3post secret/$keyName brent bbb
+}
 
-  fuser -k 8443/tcp
+c0genkey() {
+  c2get genkey/$keyName/3 evan
+}
 
+c0redisShow() {
+  echo; echo; echo + redis-cli keys 'dek:*'
+  redis-cli keys 'dek:*'
+  echo; echo + redis-cli hkeys "dek:$keyName"
+  redis-cli hkeys "dek:$keyName"
+  echo; echo + redis-cli redis hget "dek:$keyName"
+  redis-cli hget "dek:$keyName" 'secret:brent:evan'
+  redis-cli hget "dek:$keyName" 'secret:brent:henry'
+  redis-cli hget "dek:$keyName" 'secret:evan:henry'
+}
+
+c0clear() {
   redis-cli del "dek:$keyName"
+}
 
-  clientout=tmp/client.out
-  rm -f $clientout
-  ( 
-    sleep .6
-    sh scripts/genkey.sh >> $clientout
-  ) & 
-  ( 
-    sleep 2
-    sh scripts/keyinfo.sh >> $clientout
-    sleep .2
-    c1curlget load/$keyName >> $clientout
-    echo; echo "## client"
-    cat $clientout
-    c0redisShow
-   
-  ) & 
-    sleep .1
+c0client() {
+  c0genkey 
+  c0postsecret3 
+  c1get key/$keyName  
+  c1get load/$keyName 
+}
+
+c0clientTask() {
+  out=tmp/client.out
+  sleep .5
+  c0client > $out
+  sleep .2
+  echo; echo "## client output"
+  cat $out
+  c0redisShow
+}
+
+c0kill() {
+  fuser -k 8443/tcp
+}
+
+c0default() {
+  c0kill
+  c0clear
+  c0clientTask & 
     nodejs lib/app_keyserver.js | bunyan
+}
+
+if [ $# -gt 0 ]
+then
+  command=$1
+  shift
+  c$#$command $@
+else
+  c0default
+fi
+
+
 
 
